3158f0d223881cfdbc2ace1351145284f693caa4
Antonio Barile <antoniobarile13@gmail.com>
2025-08-15 17:52:02 +0200

    Add Capacitor configuration for Android build

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.antoniobarile.lunarbeautyguide',
+  appName: 'Lunar Beauty Guide',
+  webDir: 'dist'
+};
+
+export default config;
