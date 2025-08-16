0cb744f85c2cbc0b4f479234e919a76065c35d60
Antonio Barile <antoniobarile13@gmail.com>
2025-08-16 12:25:18 +0200

    Revert "Update Android launcher icon"
    This reverts commit 4e271410f802eeccf0b1d8b642b671b143bd196e.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { AndroidIconService } from './services/androidIconService'
-
-// Initialize moon phase shortcuts for Android
-AndroidIconService.initDailyUpdates();
 
 createRoot(document.getElementById("root")!).render(<App />);
